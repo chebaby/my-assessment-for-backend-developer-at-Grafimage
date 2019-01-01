55b04f93aefb45eff7227a2941a35a610c0ed96d
Nour-Eddine ECH-CHEBABY <ech.chebaby@outlook.com>
2019-01-01 14:56:57 +0100

    dummy update

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
 var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    browserSync  = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglify       = require('gulp-uglify'),
-    header       = require('gulp-header'),
-    rename       = require('gulp-rename'),
-    cssnano      = require('gulp-cssnano'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    package      = require('./package.json'),
-    wait         = require('gulp-wait'),
-    include      = require('gulp-include'),
-    babel        = require('gulp-babel');
+sass         = require('gulp-sass'),
+browserSync  = require('browser-sync'),
+autoprefixer = require('gulp-autoprefixer'),
+uglify       = require('gulp-uglify'),
+header       = require('gulp-header'),
+rename       = require('gulp-rename'),
+cssnano      = require('gulp-cssnano'),
+sourcemaps   = require('gulp-sourcemaps'),
+package      = require('./package.json'),
+wait         = require('gulp-wait'),
+include      = require('gulp-include'),
+babel        = require('gulp-babel');
 
 
 var banner = [
-  '/*!\n' +
-  ' * <%= package.name %>\n' +
-  ' * @author <%= package.author %>\n' +
-  ' * @version <%= package.version %>\n' +
-  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
-  ' */',
-  '\n'
+'/*!\n' +
+' * <%= package.name %>\n' +
+' * @author <%= package.author %>\n' +
+' * @version <%= package.version %>\n' +
+' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+' */',
+'\n'
 ].join('');
 
 
@@ -39,10 +39,10 @@ var paths = {
 };
 
 var sassOptions = {
-    
+
     errLogToConsole: true,
     includePaths    : [
-        paths.srcScss
+    paths.srcScss
     ]
 }
 
@@ -53,33 +53,33 @@ gulp.task('css', function () {
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(autoprefixer('last 4 version'))
     .pipe(cssnano())
-    //.pipe(rename({ basename: "styles", suffix: '.min' }))
-    .pipe(header(banner, { package : package }))
-    //.pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.distCss))
-    .pipe(browserSync.reload({stream:true}));
+//.pipe(rename({ basename: "styles", suffix: '.min' }))
+.pipe(header(banner, { package : package }))
+//.pipe(sourcemaps.write())
+.pipe(gulp.dest(paths.distCss))
+.pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('js',function(){
-  gulp.src([
+    gulp.src([
         paths.srcJS + 'app.js'
-    ])
+        ])
     .pipe(include({
         extensions: "js",
         hardFail: true,
         includePaths: [
-            __dirname + "/node_modules",
-            __dirname + paths.srcJS
+        __dirname + "/node_modules",
+        __dirname + paths.srcJS
         ]
     }))
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(uglify())
     .pipe(header(banner, { package : package }))
-    //.pipe(rename({ suffix: '.min' }))
-    //.pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.distJs))
-    .pipe(browserSync.reload({stream:true, once: true}));
+//.pipe(rename({ suffix: '.min' }))
+//.pipe(sourcemaps.write())
+.pipe(gulp.dest(paths.distJs))
+.pipe(browserSync.reload({stream:true, once: true}));
 });
 
 gulp.task('browser-sync', function() {
